f039166bd72ece7ce18f651e39c822eab96e0701
Jannou <jannou59@gmail.com>
2019-01-09 14:10:26 +0100

    feat(home): enrichement home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {QuizService} from "../services/quiz.service";
 import {Quiz} from "../models/quiz.model";
 import {Observable} from "rxjs";
+import {Router} from "@angular/router";
+import {LocalStorageService} from "../services/local-storage.service";
 
 @Component({
   selector: 'app-home',
@@ -12,10 +14,21 @@ export class HomeComponent implements OnInit {
 
   public quiz$: Observable<Quiz>;
 
-  constructor(private quizService: QuizService) { }
+  constructor(private quizService: QuizService,
+              private localStorageService: LocalStorageService,
+              private router: Router,
+  ) {
+  }
 
   ngOnInit() {
     this.quiz$ = this.quizService.refreshQuiz();
   }
+
+  goToLobby(id): void {
+    this.quizService.subscribe(id);
+    this.localStorageService.saveCurrentQuizId(id);
+    this.router.navigate(['/lobby']);
+  }
+
 
 }
