6bfe6a58d005f58bbe828c6fcf5e7af762b10fa3
Jannou <jannou59@gmail.com>
2019-01-09 14:09:35 +0100

    feat(services): create localStorage service

---
+++ src/app/services/local-storage.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+
+  private localStorage = window.localStorage;
+
+  constructor() { }
+
+  public findCurrentQuizId(): string {
+    return this.localStorage.getItem("quizId");
+  }
+
+  public saveCurrentQuizId(id): void {
+    this.localStorage.setItem('quizId', id)
+  }
+
+  public clearLocalStorage(): void {
+    this.localStorage.clear();
+  }
+
+}
