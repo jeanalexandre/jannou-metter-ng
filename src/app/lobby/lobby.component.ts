76c2b242f6bab6b69e3fd9a2131d9fbc82009557
Jannou <jannou59@gmail.com>
2019-01-09 14:10:59 +0100

    feat(lobby): enrichment lobby component

--- src/app/lobby/lobby.component.ts
+++ src/app/lobby/lobby.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from "rxjs";
+import {Quiz} from "../models/quiz.model";
+import {QuizService} from "../services/quiz.service";
+import {LocalStorageService} from "../services/local-storage.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-lobby',
@@ -7,9 +12,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LobbyComponent implements OnInit {
 
-  constructor() { }
+  public quiz$: Observable<Quiz>;
+
+  constructor(private quizService: QuizService,
+              private localStorageService: LocalStorageService,
+              private router: Router,
+              ) { }
 
   ngOnInit() {
+    this.quiz$ = this.quizService.refreshQuiz();
+  }
+
+  leaveLobby(id): void {
+    this.quizService.unsubscribe(id);
+    this.localStorageService.clearLocalStorage();
+    this.router.navigate(['/public']);
   }
 
 }
--- src/app/services/quiz.service.ts
+++ src/app/services/quiz.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
-import {interval, merge, Observable, of, Subject} from "rxjs";
+import {interval, merge, Observable, of} from "rxjs";
 import {Quiz} from "../models/quiz.model";
 import {HttpClient} from "@angular/common/http";
-import {distinctUntilChanged, filter, first, map, share, switchMap, takeWhile, tap} from "rxjs/operators";
+import {distinctUntilChanged, filter, map, share, switchMap} from "rxjs/operators";
 import {environment} from "../../environments/environment";
 
 @Injectable({
@@ -14,12 +14,21 @@ export class QuizService {
   }
 
   refreshQuiz(): Observable<Quiz> {
-    return merge(interval(1000), of('')).pipe(
+    return merge(interval(500), of('')).pipe(
       switchMap(() => this.httpClient.get<Quiz>(`${environment.apiBaseUrl}/quizz`)),
       map( array => array[0]),
       share(),
       filter(val => val),
       distinctUntilChanged(),
     );
+  }
+
+  subscribe(id): void{
+    this.httpClient.post(`${environment.apiBaseUrl}/quizz/${id}/subscribe`,'').subscribe( result => {
+    });
+  }
+  unsubscribe(id): void {
+    this.httpClient.post(`${environment.apiBaseUrl}/quizz/${id}/unsubscribe`,'').subscribe( result => {
+    });
   }
 }
